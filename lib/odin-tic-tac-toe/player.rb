a0bcbafa7d3abdbf8b88e040307d8eba01c7cc62
Fr0ggin <232677958+Fr0ggin@users.noreply.github.com>
2025-11-25 21:36:20 +0100

    Confining attr rights and altered win/loss/draw to handle within class.

--- lib/odin-tic-tac-toe/player.rb
+++ lib/odin-tic-tac-toe/player.rb
@@ -2,13 +2,27 @@
 
 # Player class is used to define the player used in the game class.
 class Player
-  attr_accessor :id, :name, :marker, :wins, :losses, :draws
+  attr_reader :id, :marker, :wins, :losses, :draws
+  attr_accessor :name
 
-  initialize(id, name, marker)
-  @id = id # identifies the player (1 or 2)
-  @name = name # optional but might be nice to give players a chance to input name
-  @marker = marker # "X" or "O"
-  @wins = 0 # to keep track of won games
-  @losses = 0 # to keep track of lost games
-  @draws = 0 # to keep track of draws
+  def initialize(id, name, marker)
+    @id = id # identifies the player (1 or 2)
+    @name = name # optional but might be nice to give players a chance to input name
+    @marker = marker # "X" or "O"
+    @wins = 0 # to keep track of won games
+    @losses = 0 # to keep track of lost games
+    @draws = 0 # to keep track of draws
+  end
+
+  def add_win
+    @wins += 1
+  end
+
+  def add_loss
+    @lossed += 1
+  end
+
+  def add_draw
+    @draws += 1
+  end
 end
